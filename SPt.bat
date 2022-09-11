040ac8507a0d9f6fe56dd617dcf40ee644f5c294
teboarte <60750721+teboarte@users.noreply.github.com>
2022-09-11 15:07:34 +0200

    Add files via upload

---
+++ SPt.bat
@@ -0,0 +1,2 @@
+start powershell.exe -ExecutionPolicy Bypass -File drive:\Your\Folder\SPt.ps1
+exit
---
+++ SPt.ps1
@@ -0,0 +1,15 @@
+﻿$excluded = @("iacamera64.inf","SurfaceDisplay.inf")
+$driverPath= @("drive:\filepath\to\target\folder")
+$services = Get-ChildItem -Path $driverPath -Recurse -Filter "*.inf" -Exclude $excluded
+$count=$services.Count
+ For($i = 1; $i -le $count; $i++)
+{
+ 
+Write-Progress -Activity "Install Drivers" `
+-PercentComplete (($i*100)/$count) `
+-Status "$(([math]::Round((($i)/$count * 100),0))) %"
+ 
+Start-Sleep -Milliseconds 100
+}
+Clear-Host
+$services| ForEach-Object { PNPUtil.exe /add-driver $_.FullName /install }
